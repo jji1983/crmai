2c0437aa2a6bd9cb36298d67ba89f762e9908f22
kdtduser <kdtduser@ktds-PC>
2018-09-11 13:26:06 +0900

    main 변경

--- crmai/src/main/java/com/ktds/crmai/CrmaiApplication.java
+++ crmai/src/main/java/com/ktds/crmai/CrmaiApplication.java
@@ -3,17 +3,10 @@ package com.ktds.crmai;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.ktds.crmai")
-public class CrmaiApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CrmaiApplication.class);
-	}
+public class CrmaiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmaiApplication.class, args);
